003d223555ae2e3e73cf3ef8c224f4bda87c6300
Brian Lambert <126121700+bwl8772@users.noreply.github.com>
2023-10-05 16:58:17 -0400

    Delete Topics directory

--- Topics/Printing data/From 0 to 4/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class NumberOfSpacesExample {
-    public static void main(String[] args) {
-        System.out.println("Hi!"  );
-        System.out.println("I'm a Java program.");
-    }
-}
